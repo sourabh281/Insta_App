91b04af3f871afca93bb8bc5e9e03f85c5baf397
Your Name <you@example.com>
2020-11-21 18:41:41 +0530

    Users Tab Implemented

--- app/src/main/java/com/example/insta/Users_Tab.java
+++ app/src/main/java/com/example/insta/Users_Tab.java
@@ -1,38 +1,72 @@
 package com.example.insta;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.fragment.app.Fragment;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Users_Tab extends Fragment {
+    private ListView listView;
+    private ArrayList arrayList;
+    private ArrayAdapter arrayAdapter;
+
 
 
     public Users_Tab() {
 
     }
 
-    // TODO: Rename and change types and number of parameters
-    public static Users_Tab newInstance(String param1, String param2) {
-        Users_Tab fragment = new Users_Tab();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users__tab, container, false);
+        final View view = inflater.inflate(R.layout.fragment_users__tab, container, false);
+
+        listView = view.findViewById(R.id.listView);
+        arrayList = new ArrayList();
+        arrayAdapter = new ArrayAdapter(getContext() , android.R.layout.simple_list_item_1 , arrayList);
+        final TextView txtLoadingContent = view.findViewById(R.id.txtLoadingContent);
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+
+        parseQuery.whereNotEqualTo("username" , ParseUser.getCurrentUser().getUsername());
+
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> users, ParseException e) {
+
+                if (e == null){
+
+                    if (users.size() > 0){
+
+                        for (ParseUser user : users){
+
+                            arrayList.add(user.getUsername());
+                        }
+                        listView.setAdapter(arrayAdapter);
+                        txtLoadingContent.animate().alpha(0.0f).setDuration(200);
+                        listView.setVisibility(view.VISIBLE);
+                    }
+                }
+
+            }
+        });
+
+        return  view;
+
     }
 }
